910831f0d72e8ac3045208d9972c7eb4f67e6eb9
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-05-30 04:13:24 +0530

    Make stylings of services component

--- src/components/services.js
+++ src/components/services.js
@@ -1,32 +1,81 @@
 import React from "react";
 import "../styles/services.css";
+import { FaBucket } from "react-icons/fa6";
+import { IconContext } from "react-icons";
+import { LuPackageCheck } from "react-icons/lu";
+import { FaHouse } from "react-icons/fa6";
+import { CgMenuGridR } from "react-icons/cg";
 
 const Services = () => {
   return (
-   
-        <section className="services-container">
-          <div className="individual-service-container">
-            <div className="service-icon">Image</div>
-            <div className="service-title">Title</div>
-            <div className="service-description">Description</div>
+    <section className="services-container">
+      <div className="individual-service-container">
+        <IconContext.Provider value={{ color: "white", size: "2em" }}>
+          <div className="service-icon">
+            <FaBucket />
           </div>
-          <div className="individual-service-container">
-            <div className="service-icon">Image</div>
-            <div className="service-title">Title</div>
-            <div className="service-description">Description</div>
+        </IconContext.Provider>
+        <div className="service-title">
+          <h4>IMPORT SOLUTIONS</h4>
+        </div>
+        <div className="service-description">
+          <p>
+          NVestibulum rutum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante.
+          </p>
+        </div>
+      </div>
+      <div className="individual-service-container">
+      <IconContext.Provider value={{ color: "white", size: "2em" }}>
+          <div className="service-icon">
+          <LuPackageCheck />
           </div>
-          <div className="individual-service-container">
-            <div className="service-icon">Image</div>
-            <div className="service-title">Title</div>
-            <div className="service-description">Description</div>
+        </IconContext.Provider>
+        <div className="service-title">
+          <h4>
+          EXPORT PARTNERSHIPS
+          </h4>
+        </div>
+        <div className="service-description">
+          <p>
+          NVestibulum rutum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante.
+          </p>
+        </div>
+      </div>
+      <div className="individual-service-container">
+      <IconContext.Provider value={{ color: "white", size: "2em" }}>
+          <div className="service-icon">
+          <FaHouse />
           </div>
-          <div className="individual-service-container">
-            <div className="service-icon">Image</div>
-            <div className="service-title">Title</div>
-            <div className="service-description">Description</div>
+        </IconContext.Provider>
+        <div className="service-title">
+          <h4>
+          FLEXIBLE OFFICE SOLUTIONS
+          </h4>
+        </div>
+        <div className="service-description">
+          <p>
+          NVestibulum rutum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante.
+          </p>
+        </div>
+      </div>
+      <div className="individual-service-container">
+      <IconContext.Provider value={{ color: "white", size: "2em" }}>
+          <div className="service-icon">
+          <CgMenuGridR />
           </div>
-        </section>
-
+        </IconContext.Provider>
+        <div className="service-title">
+          <h4>
+          SUPPLY CHAIN & LOGISTICS SERVICES
+          </h4>
+        </div>
+        <div className="service-description">
+          <p>
+          NVestibulum rutum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante.
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
 
