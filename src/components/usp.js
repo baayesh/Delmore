e89c1e948327282bd2782e3f9ae40c11038a1e09
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-05-30 05:47:26 +0530

    Develop ups section

--- src/components/usp.js
+++ src/components/usp.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import '../styles/usp.css';
+import React from "react";
+import "../styles/usp.css";
+import { FaHandPointRight } from "react-icons/fa";
+import usp from "../assets/usp.jpg";
 
 const USP = () => {
   return (
-    <div className='usp-main'>
-        <div className='usp-cta'>
-            <h2>
-                Heading
-            </h2>
+    <div className="usp-main">
+      <div className="usp-cta">
+        <h2>
+          Nelli Cola <br />
+          Thal & Garak Drink
+        </h2>
 
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-            </p>
+        <p>
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+          labore et dolore magna aliquyam erat, sed diam voluptua.
+        </p>
+        <p>
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor sadipscing
+          invidunt ut labore .
+        </p>
 
-            <button>
-                Click Me
-            </button>
-
-        </div>
-        <div className='usp-img'>
-            Image
-        </div>
+        <button className="usp-button">Learn More <div>
+            <FaHandPointRight />
+        </div> </button>
+      </div>
+      <div className="usp-img"><img src={usp}/></div>
     </div>
-  )
-}
+  );
+};
 
-export default USP
+export default USP;
