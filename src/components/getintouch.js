ce68e5a604e444eee64100e6aa4618a3f418f8a7
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-06 02:39:00 +0530

    Development of the get in touch area

--- src/components/getintouch.js
+++ src/components/getintouch.js
@@ -1,29 +1,30 @@
 import React from "react";
-import "../styles/getintouch.css";
+
+import "../styles/home.css";
+import { IoIosSend } from "react-icons/io";
 
 const Getintouch = () => {
   return (
     <div className="get-in-touch-container">
       <div className="get-in-touch-main">
         <div className="get-in-touch-left">
-          <h1>Get In Touch</h1>
+          <p>Monday to Friday: 9:00 AM to 5:00 PM</p>
+          <p>Saturday: 9:00 AM to 1:00 PM</p>
+          <p>Sunday: Closed</p>
+
           <h2>+64 22 0437 647</h2>
           <p>
-            252 Lichfield Street, Christchurch Central City,<br/> Christchurch 8011,
-            New Zealand
+            252 Lichfield Street, Christchurch Central City,
+            <br /> Christchurch 8011, New Zealand
           </p>
         </div>
-        <div className="line">
-
-        </div>
-        <div className="get-in-touch-right">
-            <p>
-            Monday to Friday: 9:00 AM to 5:00 PM
-            </p>
-            <p>Saturday: 9:00 AM to 1:00 PM</p>
-            <p>
-            Sunday: Closed
-            </p>
+        <div className="line"></div>
+        <div className="get-in-touch-right column">
+          <h1 className="heading">Get in touch</h1>
+          <div className="row email__submission__container">
+            <input type="email" placeholder="Email Address"></input>
+            <button><IoIosSend className="email__submission__button" /></button>
+          </div>
         </div>
       </div>
     </div>
