47a2248b1f79154799740e700917473b69a01f97
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-08 03:51:06 +0530

    make counter background

--- src/components/home/counter.js
+++ src/components/home/counter.js
@@ -16,45 +16,51 @@ const Counter = () => {
             <div className="row ">
               <div className="col counter__individual" style={{overflow:"hidden"}}>
               
-                  <h1 className="counter-number">
-                    {counterOn && (
-                      <CountUp start={0} end={10} duration={3} delay={0} />
-                    )}
-                    +
-                  </h1>
-                  <h4 className="counter-title">AWARDS</h4>
+                  <div className="column">
+                    <h6 className="counter-number counter__number">
+                      {counterOn && (
+                        <CountUp start={0} end={10} duration={3} delay={0} />
+                      )}
+                      +
+                    </h6>
+                    <h4 className="counter-title">AWARDS</h4>
+                    <p className="paragraph">Sample Paragraph</p>
+                  </div>
                
               </div>
               <div className="col counter__individual" style={{overflow:"hidden"}}>
                 
-                  <h1 className="counter-number">
+                  <h6 className="counter-number counter__number">
                     {counterOn && (
                       <CountUp start={0} end={20} duration={3} delay={0} />
                     )}
                     +
-                  </h1>
+                  </h6>
                   <h4 className="counter-title">PRODUCTS</h4>
+                  <p className="paragraph">Sample Paragraph</p>
                 
               </div>
               <div className="col counter__individual"  style={{overflow:"hidden"}}>
                 
-                  <h1 className="counter-number">
+                  <h6 className="counter-number counter__number">
                     {counterOn && (
                       <CountUp start={0} end={15} duration={3} delay={0} />
                     )}
                     +
-                  </h1>
+                  </h6>
                   <h4 className="counter-title">PARTNERS</h4>
+                  <p className="paragraph">Sample Paragraph</p>
               </div>
               <div className="col counter__individual" style={{overflow:"hidden"}}>
                 
-                <h1 className="counter-number">
+                <h6 className="counter-number counter__number">
                   {counterOn && (
                     <CountUp start={0} end={10} duration={3} delay={0} />
                   )}
                   +
-                </h1>
+                </h6>
                 <h4 className="counter-title">LOCATIONS</h4>
+                <p className="paragraph">Sample Paragraph</p>
             </div>
             </div>
           </div>
