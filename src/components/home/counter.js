2b7f53a502e18ff813db798acabc8935830d9849
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-10 08:06:13 +0530

    change counter as per the request

--- src/components/home/counter.js
+++ src/components/home/counter.js
@@ -3,7 +3,6 @@ import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 import "../../styles/home.css";
 
-
 const Counter = () => {
   const [counterOn, setCounterOn] = useState(false);
   return (
@@ -12,55 +11,58 @@ const Counter = () => {
       onExit={() => setCounterOn(false)}
     >
       <div>
-          <div className="counter-container ">
-            <div className="row ">
-              <div className="col counter__individual" style={{overflow:"hidden"}}>
-              
-                  <div className="column">
-                    <h6 className="counter-number counter__number">
-                      {counterOn && (
-                        <CountUp start={0} end={10} duration={3} delay={0} />
-                      )}
-                      +
-                    </h6>
-                    <h4 className="counter-title">AWARDS</h4>
-                  </div>
-               
-              </div>
-              <div className="col counter__individual" style={{overflow:"hidden"}}>
-                
-                  <h6 className="counter-number counter__number">
-                    {counterOn && (
-                      <CountUp start={0} end={20} duration={3} delay={0} />
-                    )}
-                    +
-                  </h6>
-                  <h4 className="counter-title">PRODUCTS</h4>
-                
-              </div>
-              <div className="col counter__individual"  style={{overflow:"hidden"}}>
-                
-                  <h6 className="counter-number counter__number">
-                    {counterOn && (
-                      <CountUp start={0} end={15} duration={3} delay={0} />
-                    )}
-                    +
-                  </h6>
-                  <h4 className="counter-title">PARTNERS</h4>
-              </div>
-              <div className="col counter__individual" style={{overflow:"hidden"}}>
-                
-                <h6 className="counter-number counter__number">
-                  {counterOn && (
-                    <CountUp start={0} end={10} duration={3} delay={0} />
-                  )}
-                  +
-                </h6>
-                <h4 className="counter-title">LOCATIONS</h4>
+        <div className="counter-container container">
+          <div className="row ">
+          <div
+              className="col counter__individual "
+              style={{ overflow: "hidden" }}
+            >
+              <h6 className="counter-number ">
+                {counterOn && (
+                  <CountUp start={0} end={20} duration={3} delay={0} />
+                )}
+                +
+              </h6>
+              <h4 className="counter-title">AWARDS</h4>
             </div>
+            <div
+              className="col counter__individual "
+              style={{ overflow: "hidden" }}
+            >
+              <h6 className="counter-number ">
+                {counterOn && (
+                  <CountUp start={0} end={20} duration={3} delay={0} />
+                )}
+                +
+              </h6>
+              <h4 className="counter-title">PRODUCTS</h4>
+            </div>
+            <div
+              className="col counter__individual"
+              style={{ overflow: "hidden" }}
+            >
+              <h6 className="counter-number ">
+                {counterOn && (
+                  <CountUp start={0} end={15} duration={3} delay={0} />
+                )}
+                +
+              </h6>
+              <h4 className="counter-title">PARTNERS</h4>
+            </div>
+            <div
+              className="col counter__individual"
+              style={{ overflow: "hidden" }}
+            >
+              <h6 className="counter-number ">
+                {counterOn && (
+                  <CountUp start={0} end={10} duration={3} delay={0} />
+                )}
+                +
+              </h6>
+              <h4 className="counter-title">LOCATIONS</h4>
             </div>
           </div>
-        
+        </div>
       </div>
     </ScrollTrigger>
   );
