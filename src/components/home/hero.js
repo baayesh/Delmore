d9d0918b1f33ff290198bd5f07b327dd7a6f8541
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-11 18:56:30 +0530

    add video to hero section

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../../styles/home.css';
+import HeroVid from '../../assets/Home/Hero_vid.mp4'
 
 const Hero = () => {
   return (
     <section className="hero">
-    <div className="hero-container container">
-      <div className="hero-content">
+    <div className="hero-container ">
+      <video className='hero__vid' src={HeroVid} autoPlay loop muted />
+  
         <h1 className="hero-title">Delmore</h1>
         <p className="hero-text">Quality-Ethics-Sustainability</p>
-      </div>
+   
     </div>
   </section>
   )
