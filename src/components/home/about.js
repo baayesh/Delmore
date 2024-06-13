456567ee5335de29bc4fa8477f042e7a2ad8c055
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-06-13 08:08:44 +0530

    add video to the about component

--- src/components/home/about.js
+++ src/components/home/about.js
@@ -3,30 +3,22 @@ import "../../styles/home.css";
 import aboutus from "../../assets/Home/A-us.png";
 import { FaHandPointRight } from "react-icons/fa";
 import { motion, useInView } from "framer-motion";
-import HeroVid from '../../assets/Home/Hero_vid.mp4';
+import HeroVid from "../../assets/Home/Hero_vid.mp4";
 
 const About = () => {
-  
-
   return (
     <div className="about-container">
       <div className="row about__container ">
         <div className="about-title column about__text__container">
-
-          <video
-            src={HeroVid} 
-            />
-
-
+          <video autoplay  src={HeroVid} />
 
           <motion.div
             initial={{ y: 100 }}
             whileInView={{ y: 0 }}
-            transition={{ duration: 1, delay:0.4 }}
-            viewport={{once:true}}
+            transition={{ duration: 1, delay: 0.4 }}
+            viewport={{ once: true }}
             className="about__content__container column container"
-
-           >
+          >
             <h5 className="heading">
               Chris Wicks Based In Los Angeles Since 2009.
             </h5>
@@ -34,23 +26,21 @@ const About = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
               tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem
               ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
+            </p>
             <motion.button
-            initial={{ x: -60, opacity: 0 }}
-            whileInView={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1, delay: 1 }}
-            viewport={{ once: true }}
-            className="about__button"
+              initial={{ x: -60, opacity: 0 }}
+              whileInView={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}
+              viewport={{ once: true }}
+              className="about__button"
             >
-            Learn More{" "}
-            <div>
-              <FaHandPointRight />
-            </div>{" "}
-          </motion.button>
+              Learn More{" "}
+              <div>
+                <FaHandPointRight />
+              </div>{" "}
+            </motion.button>
           </motion.div>
-
         </div>
-
       </div>
     </div>
   );
